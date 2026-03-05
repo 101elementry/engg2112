fa9f78a82393f39696bd95598718984ec4756743
Connor Wherry <connorwherry@gmail.com>
2026-03-05 15:57:08 +1100

    it's working

--- code/main.py
+++ code/main.py
@@ -1,12 +1,1 @@
 import torch
-
-
-# create a tensor
-x = torch.tensor([1, 2, 3])
-y = torch.tensor([4, 5, 6])
-
-# tensor addition
-z = x + y
-
-print("PyTorch version:", torch.__version__)
-print("Tensor result:", z)
