bcf4d2d1d37d79fd7433a5092e4a59b1cf53c5b9
Connor Wherry <connorwherry@gmail.com>
2026-03-05 15:55:56 +1100

    test

--- code/main.py
+++ code/main.py
@@ -1,1 +1,12 @@
-x = 0
+import torch
+
+
+# create a tensor
+x = torch.tensor([1, 2, 3])
+y = torch.tensor([4, 5, 6])
+
+# tensor addition
+z = x + y
+
+print("PyTorch version:", torch.__version__)
+print("Tensor result:", z)
