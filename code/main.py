af559297cfb85a921e8cc1f1be7e002f339409ee
Connor Wherry <connorwherry@gmail.com>
2026-03-12 14:36:43 +1100

    Kaggle public transport

--- code/main.py
+++ code/main.py
@@ -1,1 +1,9 @@
+
 import torch
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("khushikyad001/public-transport-delays-with-weather-and-events")
+
+print("Path to dataset files:", path)
